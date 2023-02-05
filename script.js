638d254f547e2aff5df0f72a3e83208aa4791d64
CharmonePinheiro <charmonepinheirorodrigues@gmail.com>
2023-02-05 10:51:19 -0300

    Insert-ok

--- script.js
+++ script.js
@@ -40,7 +40,22 @@ function openModal(edit = false, index = 0) {
     setItensBD()
     loadItens()
   }
-
+  function insertItem(item, index) {
+    let tr = document.createElement('tr')
+  
+    tr.innerHTML = `
+      <td>${item.nome}</td>
+      <td>${item.funcao}</td>
+      <td>R$ ${item.salario}</td>
+      <td class="acao">
+        <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+      </td>
+      <td class="acao">
+        <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+      </td>
+    `
+    tbody.appendChild(tr)
+  }
 
 
 
